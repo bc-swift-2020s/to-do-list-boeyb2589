31bcbbc80c0103a26f3d7987654d1139b64b3aaf
Brandon Boey <brandonjboey@gmail.com>
2020-02-09 17:39:44 -0500

    CH 5 6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Brandon Boey on 2/9/20.
+//  Copyright © 2020 Brandon Boey. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
